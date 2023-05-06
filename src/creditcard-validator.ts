9f32782fc91e8bce58727fbc26c67926ba7b9ed8
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-06 20:30:23 -0300

    docs: add jsdocs to CreditCardValidator

--- src/creditcard-validator.ts
+++ src/creditcard-validator.ts
@@ -1,4 +1,9 @@
 export class CreditCardValidator {
+    /**
+     * Validates an credit card number.
+     * @param {string} creditCard
+     * @returns {boolean}
+    */
     static validate(creditCard: string): boolean {
         const creditCardRegex = /^\d{16}/g
         return creditCardRegex.test(creditCard.replace(/\s/g, ''))
