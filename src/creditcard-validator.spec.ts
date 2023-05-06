f05af38295f583952ab4e070a435c1b3d621b137
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-06 20:27:02 -0300

    test: ensure CreditCardValidator returns false if an invalid credit card is provided

---
+++ src/creditcard-validator.spec.ts
@@ -0,0 +1,19 @@
+import { CreditCardValidator } from './creditcard-validator'
+import { type Validator } from './creditcard-validator-protocol'
+
+const makeSut = (): Validator => {
+    const sut = CreditCardValidator
+    return sut
+}
+
+describe('CreditCard Validator', () => {
+    test('should return false if an invalid credit card is provided', () => {
+        const sut = makeSut()
+        expect(sut.validate('1234 1234 1234')).toBe(false)
+    })
+
+    test('should return false if an empty credit card is provided', () => {
+        const sut = makeSut()
+        expect(sut.validate('')).toBe(false)
+    })
+})
